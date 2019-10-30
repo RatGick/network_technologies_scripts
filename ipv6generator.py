c265b027f9f4cc4e141e775f325886eb80fc3dbe
RatGick <fachion.xxx@gmail.com>
2019-10-30 23:40:03 +0200

    ipv6generator: shortening mechanism added [check description]
    - [IMPLEMENT] New IPv6 shortening feature, irrelevant zeroes are cut out in ip_address_short var
    - [UPDATE] In the copy section (lines 33-38) new lines requesting consequent adding shortened and original ip addresses into the clipboard were added
    - [FIX] Also, because CLCL have to be provided with some time frame, sleep on 0.01 seconds in 35 line was added. Import of sleep function from the time module was added

--- ipv6generator.py
+++ ipv6generator.py
@@ -4,6 +4,7 @@
 #       py ipv6generator.py
 
 from random import choice
+from time import sleep
 try:
     import pyperclip
 except ImportError:
@@ -20,8 +21,18 @@ for i in range(8):
         ip_part += digit
     ip_address.append(ip_part)
 
+ip_address_short = ip_address.copy()
+for i in range(len(ip_address_short)):
+    for _ in range(len(ip_address_short[i])-1):
+        if ip_address_short[i].startswith('0'):
+            ip_address_short[i] = ip_address_short[i][1:]
+
+
 print(':'.join(ip_address))
+print(':'.join(ip_address_short))
 try:
+    pyperclip.copy(':'.join(ip_address_short))
+    sleep(0.01)  # Because CLCL have to be provided with some time window
     pyperclip.copy(':'.join(ip_address))
 except NameError:
     pass
